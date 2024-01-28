4e520a08e493abffd21c2ecba2b4fb944f89f6ab
MartaG77 <129276573+MartaG77@users.noreply.github.com>
2024-01-28 10:42:01 +0100

    update

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,52 @@
+import ContactListItem from './ContactListItem';
+import PropTypes from 'prop-types';
+
+const ContactList = ({ setPhonebook, phonebook, filteredContacts }) => {
+  const removeContact = id => {
+    const removing = phonebook.contacts.filter(contact => contact.id !== id);
+    setPhonebook({ ...phonebook, contacts: removing });
+  };
+  return (
+    <ul>
+      {phonebook.filter === ''
+        ? phonebook.contacts?.map(item => (
+            <ContactListItem
+              key={item.id}
+              name={item.name}
+              number={item.number}
+              onRemove={removeContact}
+              id={item.id}
+            />
+          ))
+        : filteredContacts?.map(item => (
+            <ContactListItem
+              key={item.id}
+              name={item.name}
+              number={item.number}
+              onRemove={removeContact}
+              id={item.id}
+            />
+          ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+    setPhonebook: PropTypes.func.isRequired,
+    phonebook: PropTypes.shape({
+      contacts: PropTypes.array.isRequired,
+      filter: PropTypes.string.isRequired,
+    }).isRequired,
+    filteredContacts: PropTypes.array.isRequired,
+  };
+
+export default ContactList;
+
+ContactList.propTypes = {
+  setPhonebook: PropTypes.func.isRequired,
+  phonebook: PropTypes.shape({
+    contacts: PropTypes.array.isRequired,
+    filter: PropTypes.string.isRequired,
+  }).isRequired,
+  filteredContacts: PropTypes.array.isRequired,
+};
